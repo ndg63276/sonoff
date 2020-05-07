5bcee66752e207468e5ef168793f955b70e23be7
Mark Williams <24956497+ndg63276@users.noreply.github.com>
2020-05-07 13:34:45 +0100

    Make a basic web interface

--- functions.js
+++ functions.js
@@ -47,7 +47,7 @@ function login(username, password) {
 	hmac.update(nonce);
 	var hash = hmac.finalize();
 	var sign = hash.toString(CryptoJS.enc.Base64);
-	to_return['headers'] = {
+	var headers = {
 		'Authorization' : 'Sign ' + sign,
 		'Content-Type'  : 'application/json;charset=UTF-8'
 	}
@@ -55,20 +55,21 @@ function login(username, password) {
 	$.ajax({
 		url: proxyurl+url,
 		type: 'POST',
-		headers:to_return['headers'],
+		headers: headers,
 		data: JSON.stringify(app_details),
 		dataType: 'json',
 		async: false,
 		success: function (json) {
-			console.log('success');
 			console.log(json);
-			to_return['bearer_token'] = json['at'];
-			to_return['user_apikey'] = json['user']['apikey'];
-			to_return['headers']['Authorization'] = 'Bearer '+json['at']
-		},
-		error: function (json) {
-			console.log('error');
-			console.log(json);
+			if ('at' in json) {
+				to_return['bearer_token'] = json['at'];
+				setCookie('bearer_token', json['at'], 1);
+				to_return['user_apikey'] = json['user']['apikey'];
+				setCookie('user_apikey', json['user']['apikey'], 1)
+				to_return['logged_in'] = true;
+			} else {
+				to_return['logged_in'] = false;
+			}
 		}
 	});
 	return to_return;
@@ -89,51 +90,48 @@ function get_device_list(user_info) {
 		'appVersion': appVersion
 	}
 	var url = 'https://eu-api.coolkit.cc:8080/api/user/device';
+	var headers = {
+		'Authorization' : 'Bearer ' + user_info['bearer_token'],
+		'Content-Type'  : 'application/json;charset=UTF-8'
+	}
 	$.ajax({
 		url: proxyurl+url,
 		type: 'GET',
-		headers: user_info['headers'],
+		headers: headers,
 		data: query_params,
 		dataType: 'json',
 		async: false,
 		success: function (json) {
-			console.log('success');
 			console.log(json);
-			to_return = json['devicelist'];
-		},
-		error: function (json) {
-			console.log('error');
-			console.log(json);
+			if (json['error'] == 0) {
+				to_return['devices'] = json['devicelist'];
+				to_return['success'] = true;
+			} else {
+				to_return['success'] = false;
+			}
 		}
 	});
 	return to_return
 }
 
-function get_ws_info(user_info) {
-	var to_return = {};
+function get_ws_address(user_info) {
+	var to_return = '';
 	var url = 'https://eu-disp.coolkit.cc:8080/dispatch/app';
 	$.ajax({
 		url: proxyurl+url,
-		type: 'POST',
-		headers: user_info['headers'],
+		type: 'GET',
 		dataType: 'json',
 		async: false,
 		success: function (json) {
-			console.log('success');
 			console.log(json);
-			to_return = json;
-		},
-		error: function (json) {
-			console.log('error');
-			console.log(json);
+			to_return = 'wss://'+json['domain']+':'+json['port']+'/api/ws';
 		}
 	});
 	return to_return;
 }
 
-function get_ws(user_info, ws_info) {
-	ws = new WebSocket('wss://'+ws_info['domain']+':'+ws_info['port']+'/api/ws')
-
+function get_ws(user_info) {
+	ws = new WebSocket(user_info['ws_address'])
 	payload = {
 		'action'    : 'userOnline',
 		'userAgent' : 'app',
@@ -180,8 +178,77 @@ function do_login() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	user_info = login(username, password)
-	device_list = get_device_list(user_info)
-	ws_info = get_ws_info(user_info)
-	ws = get_ws(user_info, ws_info)
-	//switch_device(device_list[0], ws, 'on')
+	if (user_info['logged_in'] == true) {
+		device_list = get_device_list(user_info);
+		user_info['devices'] = device_list['devices']
+		on_login(user_info);
+	} else {
+		document.getElementById('loginfailed').innerHTML = 'Login failed';
+	}
 }
+
+function check_login(user_info) {
+	if (! user_info['bearer_token'] == '') {
+		console.log('Getting devices');
+		device_list = get_device_list(user_info);
+		return device_list;
+	} else {
+		console.log('No bearer_token');
+		return false;
+	}
+}
+
+function on_login(user_info) {
+	var login = document.getElementById('login');
+	login.classList.add('hidden');
+	var switches = document.getElementById('switches');
+	switches.classList.remove('hidden');
+	update_devices(user_info);
+	user_info['ws_address'] = get_ws_address(user_info);
+	user_info['ws'] = get_ws(user_info);
+}
+
+function update_devices(user_info, force_update) {
+	if (force_update == true) {
+		device_list = get_device_list(user_info);
+		user_info['devices'] = device_list['devices']
+	}
+	var devices = user_info['devices']
+	var switches = document.getElementById('switches');
+	switches.innerHTML = '';
+	for (device in devices) {
+		console.log(devices[device]);
+		var brand = devices[device]['brandName'];
+		var model = devices[device]['productModel'];
+		var name = devices[device]['name'];
+		var state = devices[device]['params']['switch'];
+		switches.innerHTML += '<br />'+brand+' '+model+' '+name+': ';
+		if (state == 'off') {
+			switches.innerHTML += '<a href="#" class="ui-btn ui-btn-b ui-btn-inline ui-icon-power ui-btn-icon-left" onclick="toggle('+device+');">Off</a>';
+		} else {
+			switches.innerHTML += '<a href="#" class="ui-btn ui-btn-inline ui-icon-power ui-btn-icon-left" onclick="toggle('+device+');">On</a>';
+		}
+	}
+}
+
+function toggle(device_no) {
+	var device = user_info['devices'][device_no];
+	var state = device['params']['switch'];
+	if (state == 'off') {
+		new_state = 'on';
+	} else {
+		new_state = 'off';
+	}
+	switch_device(device, user_info['ws'], new_state);
+	update_devices(user_info, true);
+}
+
+function on_logout() {
+	var switches = document.getElementById('switches');
+	switches.classList.add('hidden');
+	var login = document.getElementById('login');
+	login.classList.remove('hidden');
+}
+	//ws = get_ws(user_info, ws_info)
+	//switch_device(device_list[0], ws, 'on')
+
